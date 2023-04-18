5955b220afda78da8296d84fbad4b41f647c481c
Nikita0x <klirmio21@gmail.com>
2023-04-19 00:50:31 +0300

    button fixed

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-const scrollUpBtn = document.getElementById("scroll-up-btn");
+const mybutton = document.getElementById("scroll-up-btn");
 
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 0) {
-    scrollUpBtn.style.display = "block";
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    scrollUpBtn.style.display = "none";
+    mybutton.style.display = "none";
   }
-  console.log("page scrolled");
-});
+}
 
-scrollUpBtn.addEventListener("click", () => {
-  window.scrollTo({
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction() {
+  document.body.scrollTo({
     top: 0,
     behavior: "smooth",
-  });
-  console.log("btn clicked");
-});
+  }),
+    console.log("clicked");
+}
